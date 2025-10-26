685cde50290c098cff7bff0bf01e4c08bce903ee
hafezsalehi <hafezsaly@gmail.com>
2025-10-26 17:38:52 +0330

    new gallery1

---
+++ assets/data/galleries/gallery001.js
@@ -0,0 +1,60 @@
+// فایل JSON با اطلاعات کامل نقاشی‌ها
+const galleryData = {
+  images: [
+    {
+      id: 1,
+      title: "دریاچه کوهستانی",
+      artist: "علی رضایی",
+      city: "تهران",
+      country: "ایران",
+      year: "2023",
+      message: "حفاظت از طبیعت",
+      imageUrl:
+        "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+    },
+    {
+      id: 2,
+      title: "پل جنگلی",
+      artist: "سارا محمدی",
+      city: "اصفهان",
+      country: "ایران",
+      year: "2022",
+      message: "آلودگی هوا",
+      imageUrl:
+        "https://img.freepik.com/free-photo/beautiful-view-greenery-bridge-forest-perfect-background_181624-17827.jpg",
+    },
+    {
+      id: 3,
+      title: "آبشار ایسلندی",
+      artist: "لوکاس یوهانسن",
+      city: "ریکیاویک",
+      country: "ایسلند",
+      year: "2023",
+      message: "کمبود آب",
+      imageUrl:
+        "https://img.freepik.com/free-photo/seljalandsfoss-waterfall-during-sunset-beautiful-waterfall-iceland_335224-596.jpg",
+    },
+    {
+      id: 4,
+      title: "کوهستان در مه",
+      artist: "امیلی چن",
+      city: "شانگهای",
+      country: "چین",
+      year: "2024",
+      message: "تغییرات اقلیمی",
+      imageUrl:
+        "https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg",
+    },
+    {
+      id: 5,
+      title: "تنهایی در غروب",
+      artist: "کارلوس رویز",
+      city: "مادرید",
+      country: "اسپانیا",
+      year: "2022",
+      message: "انقراض گونه‌ها",
+      imageUrl:
+        "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+    },
+  ],
+};
